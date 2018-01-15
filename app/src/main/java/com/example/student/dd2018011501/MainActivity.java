cb8830b472be4f3b10219f018f1b0c50aa29379e
kcli2412 <kcli2412@gmail.com>
2018-01-15 10:24:11 +0800

    Volley StringRequest

--- app/src/main/java/com/example/student/dd2018011501/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018011501/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.student.dd2018011501;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -11,6 +12,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +38,26 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
+                    }
+                });
+        queue.add(request);
+        queue.start();
+    }
+
+    public void click2(View v)
+    {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest("https://www.mobile01.com/rss/news.xml",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
                     }
                 });
         queue.add(request);
